03f50c2ed0d6194a0316e957704d77636ed90ad3
Paweł Pastuszak <kotcrab@gmail.com>
2016-07-02 17:24:03 +0200

    Update Kotlin to 1.0.3

--- src/main/kotlin/com/kotcrab/szurubooru/tagger/AutoTagger.kt
+++ src/main/kotlin/com/kotcrab/szurubooru/tagger/AutoTagger.kt
@@ -499,12 +499,12 @@ class AutoTagger(private val config: ConfigDto, private val workingDir: File) {
         NewPosts("Updates new posts (having config.triggerTag)"),
         ExistingPosts("Updates already tagged posts (having config.managedTag)"),
         NewTags("Updates tags that weren't ever updated"),
-        ExistingTags ("Updates existing tags"),
-        Posts ("Updates specified posts, you must specify post ids: Posts <postId1> [postId2] [postId3] ...", true),
-        Tags ("Updates specified tags, you must specify tag names: Tags <tagName1> [tagName2] [tagName3] ...", true),
-        Notes ("Updates specified post notes only, you must specify post ids: Notes <postId1> [postId2] [postId3] ...", true),
-        BatchUpload ("Upload all image files from given directory. You must specify path to source directory: BatchUpload <path>. " +
+        ExistingTags("Updates existing tags"),
+        Posts("Updates specified posts, you must specify post ids: Posts <postId1> [postId2] [postId3] ...", true),
+        Tags("Updates specified tags, you must specify tag names: Tags <tagName1> [tagName2] [tagName3] ...", true),
+        Notes("Updates specified post notes only, you must specify post ids: Notes <postId1> [postId2] [postId3] ...", true),
+        BatchUpload("Upload all image files from given directory. You must specify path to source directory: BatchUpload <path>. " +
                 "Warning: Uploaded images will be moved to 'uploaded' subdirectory to simplify upload resuming.", true),
-        BatchDownload ("Download all images matching search query. You must specify query and optionally output directory: BatchDownload <searchQuery> [outputPath]. ", true)
+        BatchDownload("Download all images matching search query. You must specify query and optionally output directory: BatchDownload <searchQuery> [outputPath]. ", true)
     }
 }
